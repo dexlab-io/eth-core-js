e06f86570406cb0c234155585ad650f278bc3856
Parminder Klair <perminder.klair@gmail.com>
2019-05-17 15:37:03 +0530

    disable logs

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,6 +1,6 @@
 export default {
   debug: process.env.NODE_ENV === 'development',
-  ENABLE_LOGS: true,
+  ENABLE_LOGS: false,
   APP_VERSION: '0.0.1',
 
   defaultHDpathEthereum: "m/44'/60'/0'/0/0", // Compatible with Jaxx, Metamask, Exodus, imToken, TREZOR (ETH) & Digital Bitbox
